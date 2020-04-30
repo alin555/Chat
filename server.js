3ad66331f23403d549dd03b30457fcf8d7307ff9
AlinZ <alin555@gmail.com>
2020-04-30 20:38:43 +0300

    remote mongodb

--- server.js
+++ server.js
@@ -102,7 +102,11 @@ setInterval(function () {
     });
 }, 10000);
 
+let port = process.env.PORT;
+if (port== null || port == "") {
+    port=3000;
+}
 
-app.listen("3000", function () {
+app.listen(port, function () {
     console.log("server started on port 3000");
 });
